e674b5840967ce19e13daa11c1e58b17918920c6
kasireddysrikanth <kasireddysrikanth27@gmail.com>
2021-01-16 20:20:00 +0530

    url changed

--- forgotpassword.js
+++ forgotpassword.js
@@ -15,7 +15,7 @@ let checkOnSubmit = async (e) => {
    
 
 
-         let user = await fetch("https://passwordreset-12.herokuapp.com/forget-password", {
+         let user = await fetch("https://url-shortner-1.herokuapp.com/forget-password", {
 
              // Adding method type 
              method: "POST",
